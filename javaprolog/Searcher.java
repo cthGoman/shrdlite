8de383d70794539837d691707aeefd55e45e2c4e
JimStenberg <jim.stenberg@hotmail.com>
2014-03-24 18:24:30 +0100

    Searcher progress

--- javaprolog/Searcher.java
+++ javaprolog/Searcher.java
@@ -1,7 +1,22 @@
 
 
 public class Searcher{
-	public Searcher(){
-		
+	String line;
+	int pointer = 0;
+	String deliminators;
+	
+	public Searcher(String s){
+		line = s;
+	}
+	public void useDelimiter(String s){
+		deliminators = s;
+	}
+	public boolean hasNext(){
+		pointer == length(line) - 1;
+	}
+	public String next(){
+		for(int i = 0 ; i < length(deliminators) ; i++ ){
+			indexOf(, pointer)
+		}
 	}
 }
