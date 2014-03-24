e559e45e826b729e40414bec7e9bdd8815859de5
viksvens <viksvens@gmail.com>
2014-03-24 18:50:05 +0100

    Heu upd

--- javaprolog/Heuristic.java
+++ javaprolog/Heuristic.java
@@ -5,14 +5,38 @@ public class Heuristic{
 	public static int heuristic(JSONArray world, String holding, JSONArray goalWorld, String goalHolding) {
 		
 		int cost = 0;
-		
-      for (String s : world) {
-            tstrs.add(t.toString());
-      }
       
-		
-		
-		cost = 0;
+      for (int j=0; j<world.size();	j++){	
+         
+         if (world.get(j)!=goalWorld.get(j)){
+            JSONArray worldColTemp = (JSONArray) world.get(j);
+            JSONArray goalColTemp = (JSONArray) goalWorld.get(j);
+         
+
+            for (int i=0;i<worldColTemp.size();i++){
+               int tempCost = 0;            
+            
+               if(tempCost==0 && worldColTemp.get(i)==goalColTemp.get(i)){
+                  tempCost+=0;
+                  } else if (tempCost!=0 && worldColTemp.get(i)==goalColTemp.get(i)){
+                  tempCost+=4;
+                  } else if (goalColTemp.contains(worldColTemp.get(i))) {
+                  tempCost+=4;                  
+                  } else{
+                  tempCost+=2;
+                  }
+               
+               cost += tempCost;                 
+         
+            }
+         
+         }
+      
+      }
+      if(holding!=goalHolding){
+         cost++;
+      } 
+      
 		return cost;
 		
 	}
