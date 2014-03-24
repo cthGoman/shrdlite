c09590233fdde53dcf664266cc33daf8f218babd
JimStenberg <jim.stenberg@hotmail.com>
2014-03-24 18:55:04 +0100

    Searcher done

--- javaprolog/Searcher.java
+++ javaprolog/Searcher.java
@@ -12,11 +12,27 @@ public class Searcher{
 		deliminators = s;
 	}
 	public boolean hasNext(){
-		pointer == length(line) - 1;
+		return pointer < line.length();
 	}
 	public String next(){
-		for(int i = 0 ; i < length(deliminators) ; i++ ){
-			indexOf(, pointer)
+		int j = line.length();
+		for(int i = 0 ; i < deliminators.length() ; i++ ){
+			int k = line.indexOf(deliminators.charAt(i), pointer);
+			if(k >= 0){
+				j = Math.min(j, k);
+			}
 		}
+		String s;
+		if(j > pointer){
+			s = line.substring(pointer,j);
+			pointer = j;
+		}else if(j == pointer){
+			s = "" + line.charAt(pointer);
+			pointer++;
+		}else{
+			s = line.substring(pointer);
+			pointer = line.length();
+		}
+		return s;
 	}
 }
