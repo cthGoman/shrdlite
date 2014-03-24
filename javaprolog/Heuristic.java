e680136048678afd7a44a064d7d56e919f0895c5
viksvens <viksvens@gmail.com>
2014-03-24 17:39:59 +0100

    Heuristic function skal

---
+++ javaprolog/Heuristic.java
@@ -0,0 +1,19 @@
+import org.json.simple.JSONArray;
+
+public class Heuristic{
+
+	public static int heuristic(JSONArray world, String holding, JSONArray goalWorld, String goalHolding) {
+		
+		int cost = 0;
+		
+      for (String s : world) {
+            tstrs.add(t.toString());
+      }
+      
+		
+		
+		cost = 0;
+		return cost;
+		
+	}
+}
