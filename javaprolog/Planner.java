569692fbaaee6c985242e7cfc977695ac32760f3
anderssonrobert1985 <anderssonrobert1985@gmail.com>
2014-03-24 15:53:50 +0100

    asdadasd

--- javaprolog/Planner.java
+++ javaprolog/Planner.java
@@ -10,3 +10,9 @@ public class Planner{
 		return new Plan();
 	}
 }
+
+// This is how to get information about the top object in column 1:
+//JSONArray column1 = (JSONArray) world.get(0);
+//String topobject = (String) column1.get(column1.size() - 1);
+//JSONObject objectinfo = (JSONObject) objects.get(topobject);
+//String form = (String) objectinfo.get("form");
