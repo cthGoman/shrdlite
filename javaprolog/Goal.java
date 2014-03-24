7ba923910197d1d380db4935c80cebdce1c3883b
cthGoman <padam@student.chalmers.se>
2014-03-24 15:10:40 +0100

    börjat lite

--- javaprolog/Goal.java
+++ javaprolog/Goal.java
@@ -2,9 +2,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 
-public class Goal {
+public class Goal extends ArrayList{
     
-    
+    private ArrayList<Statments> = 
     
     public Goal() {
         
