a0506ba00c93a23dc3215c1618c8457ff202215a
JimStenberg <jim.stenberg@hotmail.com>
2014-03-24 19:54:59 +0100

    Tree update

--- javaprolog/Tree.java
+++ javaprolog/Tree.java
@@ -4,27 +4,22 @@ import java.util.regex.Pattern;
 
 public class Tree{
 	public Tree(String tree){
-      Node currentNode = new Node("",null);
-      String lastName;
 		Searcher sc = new Searcher(tree);
 		sc.useDelimiter("(),");
+		Node masterNode = new Node(sc.next(),null)
+      Node currentNode = masterNode;
 		while(sc.hasNext()){
 			String s = sc.next();
-			if(s != ""){
-				javax.swing.JOptionPane.showMessageDialog(null,s);
+         if(s == "("){
 			}
-         else if(s == "("){
-            currentNode = currentNode.addChild(lastName);
-         }
          else if(s == ")"){
-            currentNode.addChild(lastName);
-            currentNode = currentNode.parent;
+				currentNode = currentNode.getParent();
          }
          else if(s == ","){
-            currentNode.addChild(lastName);
+            currentNode = currentNode.getParent();
          }
          else{
-         lastName = s;
+         	currentNode = currentNode.addChild(s);
          }
          
           
