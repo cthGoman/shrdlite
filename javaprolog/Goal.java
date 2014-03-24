c5e0142acb60db09b7a696f32080d7f94bc13ae1
turburk <turburk@gmail.com>
2014-03-24 15:35:51 +0100

    Goal
    method toString Added

--- javaprolog/Goal.java
+++ javaprolog/Goal.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Goal{
     
-    private ArrayList<ArrayList<Statement>> = new ArrayList<new ArrayList<Statement>()>()
-    
+    //private ArrayList<ArrayList<Statement>>  = new ArrayList<new ArrayList<Statement>()>()
+    private ArrayList<ArrayList<Statement>> PDDL;
     public Goal() {
         
     }
-    
+    public String ToString()
+    {
+      return "true";
+    }
 }
