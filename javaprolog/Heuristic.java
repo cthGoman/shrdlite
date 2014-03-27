a0d8a64e260843d7827e08ecaaff10ce721faa53
viksvens <viksvens@gmail.com>
2014-03-27 13:05:53 +0100

    Heuristic med comments

--- javaprolog/Heuristic.java
+++ javaprolog/Heuristic.java
@@ -1,19 +1,19 @@
 import org.json.simple.JSONArray;
-import java.util.List;
-import java.util.ArrayList;
 
-public class Heuristic extends ArrayList<Integer>{
+public class Heuristic{
    private int cost;
    private int colCost;
+
 
 	public Heuristic(JSONArray world, String holding, JSONArray goalWorld, String goalHolding) {
 		
 		cost = 0;
       colCost = 0;
-      ArrayList<Integer> heuristic = new ArrayList(2);
-                  
+      
+      //Loop over all columns                 
       for (int j=0; j<world.size();	j++){	
       
+          //If the column differs check differences
           if (!world.get(j).equals(goalWorld.get(j))){
             JSONArray worldColTemp = (JSONArray) world.get(j);
             JSONArray goalColTemp = (JSONArray) goalWorld.get(j);
@@ -21,35 +21,44 @@ public class Heuristic extends ArrayList<Integer>{
             int tempCost = 0;
             
             for (int i=0;i<worldColTemp.size();i++){
-                           
+               
+               
+               // If the object in world is in the right column            
                if(goalColTemp.contains(worldColTemp.get(i))){
                   if(tempCost!=0){
-                     tempCost+=4;
+                     tempCost+=4; //If something below the object needs to be moved
                   }
                   else if(worldColTemp.size()>=i){
                      if(worldColTemp.get(i)!=goalColTemp.get(i)){
-                        tempCost+=4;
+                        tempCost+=4; //If the object is in the right column but in the wrong place
                      }
                   }
                
                }
               else if(!goalHolding.isEmpty()){ 
                    if(goalHolding.equals(worldColTemp.get(i))){
-                  tempCost+=1;
+                  tempCost+=1; //If the object should be in holding
                    }   
                }
               else{
-                  tempCost+=2;
+                  tempCost+=2; //If the object is in the wrong column
               }
             
             }
             
             if(tempCost>colCost){
-               colCost=tempCost;
+               colCost=tempCost; //The column with the highest cost
             }
-            cost += tempCost;
+            
+            cost += tempCost; //Add the column cost to the total cost
          
           }
+      
+      if(!holding.isEmpty()){
+         
+      }
+      
+      
       
       }
 
