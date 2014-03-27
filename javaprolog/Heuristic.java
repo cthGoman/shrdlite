42a50336c35a8bcda8ba593f1871bb248065c922
cthGoman <padam@student.chalmers.se>
2014-03-27 09:55:29 +0100

    huer

--- javaprolog/Heuristic.java
+++ javaprolog/Heuristic.java
@@ -12,24 +12,21 @@ public class Heuristic{
             JSONArray worldColTemp = (JSONArray) world.get(j);
             JSONArray goalColTemp = (JSONArray) goalWorld.get(j);
          
-
+            int tempCost = 0;
             for (int i=0;i<worldColTemp.size();i++){
-               int tempCost = 0;            
+                           
             
                if(tempCost==0 && worldColTemp.get(i)==goalColTemp.get(i)){
                   tempCost+=0;
-                  } else if (tempCost!=0 && worldColTemp.get(i).equals(goalColTemp.get(i))){
+               } else if (tempCost!=0 && worldColTemp.get(i).equals(goalColTemp.get(i))){
                   tempCost+=4;
-                  } else if (goalColTemp.contains(worldColTemp.get(i))) {
+               } else if (goalColTemp.contains(worldColTemp.get(i))) {
                   tempCost+=4;                  
-                  } else{
+               } else{
                   tempCost+=2;
-                  }
-               
-               cost += tempCost;
-                              
-         
+               }
             }
+            cost += tempCost;
          
           }
       
