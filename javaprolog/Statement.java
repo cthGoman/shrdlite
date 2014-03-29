5989725ee85ad9904c900ca5d72bc96211176e59
JimStenberg <jim.stenberg@hotmail.com>
2014-03-29 17:14:29 +0100

    Concept method for generating goal
    statement slightly changed and note added

--- javaprolog/Statement.java
+++ javaprolog/Statement.java
@@ -5,8 +5,13 @@ public class Statement extends ArrayList<String>{
 
 	
 	public Statement(String spacialOperator,String arg1,String arg2){
-      this.add(spacialOperator);
-		this.add(arg1);
-		this.add(arg2);
+      super.add(spacialOperator);
+		super.add(arg1);
+		super.add(arg2);
+	}
+	// This prevents anyone from adding an additional string to the statement.
+	// Should also be done for a lot of other methods in arraylist, but nevermind
+	public boolean add(String s){
+		return false;
 	}
 }
