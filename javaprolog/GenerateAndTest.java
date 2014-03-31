9e03055a08c46cb60b092806852920245620e5ca
viksvens <viksvens@gmail.com>
2014-03-31 14:59:39 +0200

    aasdfg

--- javaprolog/GenerateAndTest.java
+++ javaprolog/GenerateAndTest.java
@@ -1,15 +1,31 @@
 import java.util.List;
 import java.util.ArrayList;
-import org.json.simple.JSONArray;
+import org.json.simple.*;
 
 public class GenerateAndTest{
 
-   public static JSONArray generateWorld(Goal goals, JSONArray world){
-   JSONArray goalWorld = (JSONArray) WorldCopy.copy(world);
+   public static JSONArray generateWorld(Goal goal, JSONArray world, JSONObject objectsIn){
+   JSONArray goalWorld = (JSONArray) WorldFunctions.copy(world);
    
-   for(int i=0, i < goals.size(), i++){
-      if goals.get(i)
+   for(int i=0; i < goal.size(); i++){ //Loop over rows in goal
       
+      for(int j=0; j<goal.get(i).size(); j++){ //Loop over columns in goal
+         
+         if (goal.get(i).get(j).get(0).equals("ontop")){ //If it is an ontop statement
+            
+            for(int k=0; k<world.size(); k++){   //Loop over columns in the world
+            
+            //Check if the column contains any of the objects in the statement
+               if(WorldFunctions.worldColumnContains(world, goal.get(i).get(j).get(1), k) ||
+                  WorldFunctions.worldColumnContains(world, goal.get(i).get(j).get(2), k) ||
+                  goal.get(i).get(j).get(1).contains(""+k)){
+               
+               }
+            
+            }
+         
+         }
+      }
    }
    
    return goalWorld;
