f4a8a0786f5f2edaf0bae34244e216e63986e1d4
anderssonrobert1985 <anderssonrobert1985@gmail.com>
2014-03-31 11:53:16 +0200

    Ny constraint
    Bollar får inte ligga under andra objekt

--- javaprolog/Constraints.java
+++ javaprolog/Constraints.java
@@ -1,21 +1,49 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import java.util.*;
 
 
 public class Constraints{
 
-	public static boolean isAllowed(JSONArray world, String holding, JSONObject objects) {
-	   
+	public static boolean isWorldAllowed(JSONArray world, String holding, JSONObject objects) {
+
       boolean isAllowed = true;
-      
       isAllowed = (holding==null || holding.length()<2);
-      
-      for(int i=0;i<world.length();i++){
-      
-      
+      //System.out.println(world);
+      for(int i=0; i<world.size(); i++){
+         JSONArray column = (JSONArray) world.get(i);
+         if(column.size()>1){
+            for(int j=0; j<column.size(); j++){
+               String object = (String) column.get(j);
+               JSONObject objectinfo = (JSONObject) objects.get(object);
+               String form = (String) objectinfo.get("form");
+           
+               if(form.contains("ball")){
+                  if(j+1<column.size()){
+                     return false;
+                     //System.out.println("FAIL, ball can not hold objects");
+                  }
+               }
+            }
+         }
       } 
-      
       return isAllowed;
 		
 	}
+   
+   public static boolean isGoalRowAllowed(List<Statement> goalRow, JSONObject objects){
+      boolean isAllowed = true;
+      for (Statement statement:goalRow){
+         // if(statement.get(0)=="ontop"){
+//             if(objects.statement.get(2).form == "ball"){
+//                return false;
+//             }
+//             if((objects.statement.get(1).form == "ball") && !(objects.statement.get(2).contains("floor"))){
+//                return false;
+//             }
+//          
+//          }      
+      }
+      return isAllowed;
+   }
 }
