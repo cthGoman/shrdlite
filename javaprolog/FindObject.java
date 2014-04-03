e583470a01aa44f1597178de05158b1197b53660
JimStenberg <jim.stenberg@hotmail.com>
2014-04-03 10:15:48 +0200

    Added FindObject
    static method match is currently stupid

---
+++ javaprolog/FindObject.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import org.json.simple.JSONObject;
+
+public class FindObject{
+	public static ArrayList<String> match(List<String> object,JSONObject objectsInformation, ArrayList<ArrayList<String>> world){
+		return new ArrayList<String>();
+//		JSONObject objectinfo = (JSONObject) objects.get(topobject);
+//		String form = (String) objectinfo.get("form");
+	}
+}
