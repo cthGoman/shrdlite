3b91471320006df55c72102ff62f751b7be1ae85
turburk <turburk@gmail.com>
2014-04-03 10:42:59 +0200

    Reations

---
+++ javaprolog/Relations.java
@@ -0,0 +1,37 @@
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Relations{
+
+
+public static ArrayList<ArrayList<Statement>> relations(JSONObject objects,ArrayList<ArrayList<String>> world,Tree tree, ArrayList<String> object0){
+
+
+        // This is how to get information about the top object in column 1:
+        //JSONArray column1 = (JSONArray) world.get(0);
+        //String topobject = (String) column1.get(column1.size() - 1);
+        //JSONObject objectinfo = (JSONObject) objects.get(topobject);
+        //String form = (String) objectinfo.get("form");
+
+String topobject = world.get(0).get(0);
+JSONObject objectinfo = (JSONObject) objects.get(topobject);
+String color = (String) objectinfo.get("color");
+DebugFile.println(color);
+
+
+
+
+
+return new ArrayList<ArrayList<Statement>>(); 
+
+
+}
+
+
+
+
+
+}
