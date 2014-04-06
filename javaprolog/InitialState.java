eefe622d6ab69e62db09ac01e35f9135230f1e5b
JimStenberg <jim.stenberg@hotmail.com>
2014-04-06 14:36:55 +0200

    Preliminary done with InitialState
    some debugfile.print remains

--- javaprolog/InitialState.java
+++ javaprolog/InitialState.java
@@ -4,24 +4,28 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
 public class InitialState{
-	public static ArrayList<String> getInitialObjects(Node masterNode, JSONObject objectsInformation, ArrayList<ArrayList<String>> world){
-		Node currentNode = masterNode.getChild(0);
-		int k = 0;
-		int l = Math.min(currentNode.getValue().length(),8);
-DebugFile.println(currentNode.getValue().substring(0,l) + "\t" + k);
-		while(currentNode.getValue().substring(0,l).equals("relative")){
-			currentNode = currentNode.getLastChild();
-			k++;
-			l = Math.min(currentNode.getValue().length(),8);
-DebugFile.println(currentNode.getValue().substring(0,l) + "\t" + k);
+   public static ArrayList<String> getInitialObjects(Node masterNode, JSONObject objectsInformation, ArrayList<ArrayList<String>> world){
+      Node currentNode = masterNode.getChild(0);
+      int k = 0;
+      int l = Math.min(currentNode.getValue().length(),8);
+      DebugFile.println(currentNode.getValue().substring(0,l) + "\t" + k);
+      while(currentNode.getValue().substring(0,l).equals("relative")){
+         currentNode = currentNode.getLastChild();
+         k++;
+         l = Math.min(currentNode.getValue().length(),8);
+         DebugFile.println(currentNode.getValue().substring(0,l) + "\t" + k);
+      }
+      ArrayList<String> oldObjects = FindObject.match(currentNode.createObject(),objectsInformation,world);
+   	currentNode = currentNode.getParent();
+      String relation = currentNode.getChild(0).getValue();
+      currentNode = currentNode.getParent();
+		while(currentNode != null){
+			ArrayList<String> newObjects = FindObject.match(currentNode.createObject(),objectsInformation,world);
+			oldObjects = FindObject.relatedObject(newObjects,relation,oldObjects,world);
+			currentNode = currentNode.getParent();
+			relation = currentNode.getChild(0).getValue();
+			currentNode = currentNode.getParent();
 		}
-		ArrayList<String> oldObjects = FindObject.match(currentNode.createObject(),objectsInformation,world);
-		currentNode = currentNode.getParent();
-		String relation = currentNode.getChild(0).getValue();
-DebugFile.println(relation);
-		currentNode = currentNode.getParent();
-		ArrayList<String> newObjects = FindObject.match(currentNode.createObject(),objectsInformation,world);
-		oldObjects = FindObject.relatedObject(newObjects,relation,oldObjects,world);
-		return new ArrayList<String>();
-	}
+		return oldObjects;
+   }
 }
