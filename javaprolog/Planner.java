c78576c125484ee639d876f7fc9ab41638cc0366
cthGoman <padam@student.chalmers.se>
2014-04-07 10:09:21 +0200

    uppdaterat efter merge

--- javaprolog/Planner.java
+++ javaprolog/Planner.java
@@ -146,15 +146,15 @@ public class Planner{
       }
 		return plan;
 	}
-<<<<<<< HEAD
-}
+
+
 
 // This is how to get information about the top object in column 1:
 //JSONArray column1 = (JSONArray) world.get(0);
 //String topobject = (String) column1.get(column1.size() - 1);
 //JSONObject objectinfo = (JSONObject) objects.get(topobject);
 //String form = (String) objectinfo.get("form");
-=======
+
    
    
    
@@ -350,4 +350,4 @@ public class Planner{
       return plan;
    }
 }
->>>>>>> origin/planner
+
