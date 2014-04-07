ce2f3111e8a1bf60512925321959b03966dc250a
viksvens <viksvens@gmail.com>
2014-04-07 10:56:39 +0200

    setCost upd

--- javaprolog/setCost.java
+++ javaprolog/setCost.java
@@ -3,14 +3,17 @@ import java.util.List;
 import java.util.ArrayList;
      
 public class SetCost{
-private ArrayList<Integer> objCosts;
-private JSONObject obj;
+HashMap<String, Integer> costMap;
       
 
       
-   public SetCost(JSONObject objects,ArrayList<Integer> costList) {
-      obj = objects;
-      objCosts = costList;
+   public SetCost(JSONArray world, String holding) {
+      
+      ArrayList<String> objects = getAllObjectsInWorld(world, holding);
+      costMap = new HashMap(objects.size());
+      for(String obj : objects){
+   	   costMap.put(obj, new Integer(0));
+   	}
    }
          
    public SetCost(JSONObject objects,Integer initCost) {
@@ -24,5 +27,18 @@ private JSONObject obj;
    public int evaluateMove(String object, JSONArray col){
    
    return 0;
+   }
+   
+   public void moveAbove(String object, JSONArray col){
+      boolean above = false;
+      for(int i=0; i<col.size(); i++){
+         if(above){
+            
+         }
+         else if(((String)col.get(i)).contains(object)){
+            above = true;
+         }
+      }
+   
    }
 }
