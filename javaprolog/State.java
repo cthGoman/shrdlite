a57c0b8a88eda45d72f5eaf66cf1c5ffbb6a3ea0
anderssonrobert1985 <anderssonrobert1985@gmail.com>
2014-04-10 11:16:01 +0200

    skfndjifndndeskfnoefudrhgfuhedjuf

--- javaprolog/State.java
+++ javaprolog/State.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 import org.json.simple.JSONArray;
 
 public class State{
-   private ArrayList world;
+   //private ArrayList world;
    private String holding;
    
    public State(JSONArray world, String holding){
         this.holding = holding;
+        //for(JSONArray column : world){
         
+        //}
    }
+}
