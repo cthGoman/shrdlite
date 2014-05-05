86f7820df9b650b08a63dec4ff6e15504c78d327
JimStenberg <jim.stenberg@hotmail.com>
2014-05-05 15:17:15 +0200

    Documentation Example

--- javaprolog/DebugFile.java
+++ javaprolog/DebugFile.java
@@ -1,9 +1,13 @@
 import java.io.*;
 import java.util.*;
 
+/**Creats files in the DebugFile folder with prints from the program.<br>
+*This class shouldn't be used anywhere in the final version.*/
 public class DebugFile{
 	private static int active = 0;
 	private static LinkedList<PrintWriter> writers = new LinkedList<PrintWriter>();
+	/**Initalize a new file to write to.<br>
+	*only one can be open at a time*/
 	public static void start(){
 		try{
 			active = writers.size();
@@ -13,12 +17,18 @@ public class DebugFile{
 			javax.swing.JOptionPane.showMessageDialog(null,e);
 		}
 	}
+	/**Print a String to file without adding new line.
+	*@param s The string to be printed.*/
 	public static void print(String s){
 		writers.get(active).print(s);
 	}
+	/**Print a String to file and adding new line.
+	*@param s The string to be printed.*/
 	public static void println(String s){
 		writers.get(active).println(s);
 	}
+	/**Close a file and makes all output to it.<br>
+	*Must be done before execution is stopped, or no info will appear in the file.*/
 	public static void stop(){
 		writers.get(active).flush();
 		writers.get(active).close();
